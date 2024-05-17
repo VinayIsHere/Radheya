67b346d65cd56100ba97194eb90cbd30accfddff
VinayIsHere <vk21677@gmail.com>
2024-05-17 16:01:38 +0530

    Revert "initial commit"
    This reverts commit ff03e7bced5f1984ff8415c50c9f0182994acd15.

--- frontend/public/preload.cjs
+++
@@ -1,4 +0,0 @@
-/* eslint-disable no-undef */
-const {ipcRenderer, contextBridge} = require('electron');
-contextBridge.exposeInMainWorld("ipc",ipcRenderer)
-
--- frontend/src/httpService/apiEndPoints.js
+++
@@ -1,7 +0,0 @@
-import env from "../../config/index.json";
-const ApiEndPoints = {
-  recordingModes: `${env.baseUrl}/recordingModes`,
-  startRecording: `${env.baseUrl}/startrecording`,
-};
-
-export default ApiEndPoints;
